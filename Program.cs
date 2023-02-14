2a4a2f99506890628afb920ca3e4cf422723d1d1
rshunter@crimson.ua.edu <122571682+shims310@users.noreply.github.com>
2023-02-14 14:16:13 -0600

    rough outline of project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace menu;
+class program
+{
+//Global variables
+    static void Main(string[] args)
+    {
+        public static class Globals
+        {
+            public const symbol = "*";
+        }
+    }
+//start main
+        Menu();
+        GetFull();
+        GetPartial();
+ //end main
+
+        //Menu
+        static Menu()
+        {
+             System.Console.WriteLine("What type of triangle would you like to display?");
+             System.Console.WriteLine("  -   Enter 1 to generate a full triangle");
+             System.Console.WriteLine("  -   Enter 2 to generate a partial triangle");
+             System.Console.WriteLine("  -   Enter 3 to exit the program");
+             menuInput = System.Console.ReadLine();
+        }
+        //GetFull
+        static void GetFull()
+        {
+            if(menuInput )
+        }
+
+        //getPartial
+        static void GetPartial()
+        {
+
+        }
+
+    } 
+}
