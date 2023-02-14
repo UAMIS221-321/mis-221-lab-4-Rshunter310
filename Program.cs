1415bb5f53eac7582050e6a451e99e6e5d6654ab
rshunter@crimson.ua.edu <122571682+shims310@users.noreply.github.com>
2023-02-14 16:06:41 -0600

    final version

--- Program.cs
+++ Program.cs
@@ -1,41 +1,99 @@
 ﻿using System;
-namespace menu;
-class program
+namespace DrawTriangle
 {
-//Global variables
-    static void Main(string[] args)
+    static class Program
     {
-        public static class Globals
-        {
-            public const symbol = "*";
-        }
-    }
-//start main
-        Menu();
-        GetFull();
-        GetPartial();
- //end main
+        // Global variables
+        const string symbol = "*";
+        static readonly Random random = new Random();
 
-        //Menu
-        static Menu()
+        // Main method
+        static void Main(string[] args)
         {
-             System.Console.WriteLine("What type of triangle would you like to display?");
-             System.Console.WriteLine("  -   Enter 1 to generate a full triangle");
-             System.Console.WriteLine("  -   Enter 2 to generate a partial triangle");
-             System.Console.WriteLine("  -   Enter 3 to exit the program");
-             menuInput = System.Console.ReadLine();
+            bool exit = false;
+            while (!exit)
+            {
+                int menuChoice = Menu();
+                if (menuChoice == 1)
+                {
+                    GetFull();
+                }
+                else if (menuChoice == 2)
+                {
+                    GetPartial();
+                }
+                else if (menuChoice == 3)
+                {
+                    exit = true;
+                }
+                else
+                {
+                    System.Console.WriteLine("Invalid input, please try again.");
+                }
+            }
+            System.Console.WriteLine("Thank you for using my program :) ");
         }
-        //GetFull
+
+        // Display menu and get user input
+        static int Menu()
+        {
+            System.Console.WriteLine("What type of triangle would you like to display?");
+            System.Console.WriteLine("  - Enter 1 to generate a full triangle");
+            System.Console.WriteLine("  - Enter 2 to generate a partial triangle");
+            System.Console.WriteLine("  - Enter 3 to exit the program");
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out int choice))
+            {
+                choice = 0;
+            }
+            return choice;
+        }
+        // Generate a full triangle
         static void GetFull()
         {
-            if(menuInput )
+            int length = random.Next(3, 10);
+            int i = 0;
+            while (i < length)
+            {
+                bool symbolPresent = false;
+                for (int j = 0; j <= i; j++)
+                {
+                System.Console.Write(symbol);
+                symbolPresent = true;
+                }
+                if (symbolPresent)
+                {
+                    System.Console.WriteLine();
+                }
+                i++;
+            }
         }
-
-        //getPartial
-        static void GetPartial()
-        {
-
+        // Generate a partial triangle
+       static void GetPartial()
+       {
+            int length = random.Next(3, 10);
+            int height = random.Next(3, length+1); //length has to be 1 more than height
+            for (int i = 0; i < height; i++)
+            {
+                bool symbolPresent = false;
+                for (int j = 0; j <= i; j++)
+                {
+                    if (random.Next(2) == 0 || j == i)
+                    {
+                        System.Console.Write(symbol);
+                        symbolPresent = true;
+                    }
+                    else
+                    {
+                        System.Console.Write(" ");
+                    }
+                }
+                if (!symbolPresent) //prevents blank triangle being made
+                {
+                    System.Console.Write(symbol);
+                }
+                System.Console.WriteLine();
+            }
         }
-
-    } 
+    }
 }
